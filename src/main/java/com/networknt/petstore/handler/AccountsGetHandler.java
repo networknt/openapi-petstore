d11279dec58893e47b35bc893d24fc7e52dce3a0
Steve Hu <stevehu@gmail.com>
2024-12-21 13:52:27 -0500

    update for accounts

---
+++ src/main/java/com/networknt/petstore/handler/AccountsGetHandler.java
@@ -0,0 +1,32 @@
+package com.networknt.petstore.handler;
+
+import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
+
+import com.networknt.handler.LightHttpHandler;
+import com.networknt.http.HttpMethod;
+import com.networknt.http.HttpStatus;
+import com.networknt.http.MediaType;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+import io.undertow.util.HeaderMap;
+
+import java.util.Deque;
+import java.util.Map;
+
+/**
+ * This handler is responsible for getting all accounts from the database.
+ */
+public class AccountsGetHandler implements LightHttpHandler {
+
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        // HeaderMap requestHeaders = exchange.getRequestHeaders();
+        // Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
+        String responseBody = "[{\"accountNo\":123,\"userId\":\"sh35\",\"accountType\":\"C\",\"firstName\":\"Steve\",\"lastName\":\"Hu\",\"status\":\"O\"},{\"accountNo\":456,\"userId\":\"sh35\",\"accountType\":\"B\",\"firstName\":\"Steve\",\"lastName\":\"Hu\",\"status\":\"O\"},{\"accountNo\":789,\"userId\":\"sh35\",\"accountType\":\"S\",\"firstName\":\"Steve\",\"lastName\":\"Hu\",\"status\":\"O\"},{\"accountNo\":101,\"userId\":\"sh35\",\"accountType\":\"S\",\"firstName\":\"Steve\",\"lastName\":\"Hu\",\"status\":\"C\"}]";
+        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        exchange.setStatusCode(HttpStatus.OK.value());
+        exchange.getResponseSender().send(responseBody);
+    }
+}
---
+++ src/main/java/com/networknt/petstore/model/Account.java
@@ -0,0 +1,118 @@
+package com.networknt.petstore.model;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Account  {
+
+    private String accountNo;
+    private String ownerId;
+    private String accountType;
+    private String firstName;
+    private String lastName;
+    private String status;
+
+    public Account () {
+    }
+
+    @JsonProperty("accountNo")
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    @JsonProperty("ownerId")
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    @JsonProperty("accountType")
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    @JsonProperty("firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonProperty("lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @JsonProperty("status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Account Account = (Account) o;
+
+        return Objects.equals(accountNo, Account.accountNo) &&
+               Objects.equals(ownerId, Account.ownerId) &&
+               Objects.equals(accountType, Account.accountType) &&
+               Objects.equals(firstName, Account.firstName) &&
+               Objects.equals(lastName, Account.lastName) &&
+               Objects.equals(status, Account.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNo, ownerId, accountType, firstName, lastName, status);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class Account {\n");
+        sb.append("    accountNo: ").append(toIndentedString(accountNo)).append("\n");        sb.append("    ownerId: ").append(toIndentedString(ownerId)).append("\n");        sb.append("    accountType: ").append(toIndentedString(accountType)).append("\n");        sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");        sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");        sb.append("    status: ").append(toIndentedString(status)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
